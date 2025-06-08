a8826c42405b6adf215981b6dfdfd36dfcd134b8
amritaup <amrita.success@gmail.com>
2025-06-08 13:06:16 +0530

    some more changes

--- code_my_python.py
+++ code_my_python.py
@@ -1,3 +1,5 @@
 print("This is the first file ")
-for i in range(1,10):
-    print(i)
+list=[1,2,3,33,440,100]
+for i in list:
+    if i%10==0:
+        print("The list of numbers ending with 0 are ", i)
